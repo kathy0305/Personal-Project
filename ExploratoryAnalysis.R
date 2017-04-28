2ebb883078d0b2858771e091523b3a082547c405
kathy0305 <kathy0305@hotmail.com>
2017-04-27 22:56:10 -0400

    Added more plots

--- ExploratoryAnalysis.R
+++ ExploratoryAnalysis.R
@@ -35,4 +35,35 @@ ggplot(dfExp, aes(x=year,y= ATTENDANCE))+
     geom_smooth()
 ## that didnt tell me much
 
+## How many parks in each region
+ByRegion <- dfExp %>%
+    group_by(Region)%>%
+    summarize(count= n())
+
+## Total Attendance for each region
+ByRegionSum <-dfExp %>%
+    group_by(Region) %>%
+    summarize(AttdByRegion= sum(ATTENDANCE))
+
+## Total Attendance for each region ranked
+ByRegionSum <-dfExp %>%
+    group_by(Region) %>%
+    summarize(AttdByRegion= sum(ATTENDANCE)) %>%
+    arrange(desc(AttdByRegion)) ##arrange by largest 
+
+
+## having a problem with the arrangment of x-axis
+## The bars are not dsplaying in desc order
+ggplot(ByRegionSum,aes(x=Region, y=AttdByRegion))+
+  #  coord_cartesian(ylim = c(1000000, 100000000))+
+    #discrete_scale(ylim = c(1000000, 100000000))+
+    #scale_y_continuous(limits=c(1000000, 100000000), oob=rescale_none)
+    geom_bar(stat ="identity")
+    
+
+##By default, geom_bar uses stat="count" which makes the height of the bar
+##proportion to the number of cases in each group (or if the weight 
+##aethetic is supplied, the sum of the weights).
+##If you want the heights of the bars to represent values in the data, 
+##use stat="identity" and map a variable to the y aesthetic.
     
